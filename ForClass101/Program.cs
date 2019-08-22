65e3a70a4d657803156f5c41785aa7721d452c58
김대성 <daeseong@gaonmaru.kr>
2019-08-22 20:48:24 +0900

    초기 커밋.

---
+++ ForClass101/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForClass101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
