bc0e4d9d61ca5308d2cbfcf1692b757ef8260aae
KDS5601 <igs8833@hotmail.com>
2019-08-25 23:07:17 +0900

    코드 C#으로 번역완료 파일분리 파생 메소드 일원화

--- ForClass101/Program.cs
+++ ForClass101/Program.cs
@@ -8,95 +8,26 @@ namespace ForClass101
         static void Main(string[] args)
         {
             List<Vehicle> road = new List<Vehicle>();
-            List<Vehicle.Wheel> wheels = new List<Vehicle.Wheel>();
 
-            Car car = new Car(4, , 100);
+            Vehicle.Wheel[] wheelsCar = { new Vehicle.Wheel("rubber"), new Vehicle.Wheel("rubber"), new Vehicle.Wheel("rubber"), new Vehicle.Wheel("rubber") };
+            Vehicle.Wheel[] wheelsBike = { new Vehicle.Wheel("plastic"), new Vehicle.Wheel("plastic") };
 
-            Bike bike = new Bike(2, [new Wheel("plastic"), new Wheel("plastic")], 50);
+            Car car = new Car(wheelsCar, 100);
 
-            road.push(car);
-            road.push(bike);
-            car.run();
-            bike.start();
+            Bike bike = new Bike(wheelsBike, 50);
 
-            console.log("All vehicles on road: ", road);
-            console.log("isEmpty: ", car.isRoadEmpty(road));
+            road.Add(car);
+            road.Add(bike);
+            car.Run();
+            bike.Start();
 
-        }
-    }
-
-    public class Vehicle
-    {
-        private int numberOfWheels; // bigger than -1
-        public List<Wheel> wheels = new List<Wheel>();
-        private int fuel; // 0~100
-
-        public Vehicle(int numberOfWheels, ref List<Wheel> wheels, int fuel)
-        {
-            this.numberOfWheels = numberOfWheels;
-            this.wheels = wheels;
-            this.fuel = fuel;
-        }
-        public int getNumberOfWheels()
-        {
-            return this.numberOfWheels;
-        }
-
-        public int getFuel()
-        {
-            return this.fuel;
-        }
-
-        public class Wheel
-        {
-            public string kind;
-            public int rpm = 0;
-
-            public Wheel(string kind_p)
+            Console.Write("All vehicles on road: ");
+            foreach (Vehicle vehicle in road)
             {
-                this.kind = kind_p;
+                Console.Write(vehicle.GetType().ToString() + " ");
             }
-
-            public void setNewRPM(int r)
-            {
-                this.rpm = r;
-            }
-        }
-    }
-
-    class Car : Vehicle
-    {
-        public Car(int numberOfWheels, ref List<Wheel> wheels, int fuel) : base(numberOfWheels, ref wheels, fuel) { }
-
-        // 시동을 켠다
-        public void run()
-        {
-            for (int i = 0; i < this.getNumberOfWheels(); i++)
-            {
-                this.wheels[i].rpm = 5;
-            }
-        }
-        public void isRoadEmpty(ref List<Vehicle> vehicles)
-        {
-            vehicles.Clear();
-        }
-    }
-
-    class Bike : Vehicle
-    {
-        public Bike(int numberOfWheels, ref List<Wheel> wheels, int fuel) : base(numberOfWheels, ref wheels, fuel) { }
-
-        // 페달을 밟는다
-        public void start()
-        {
-            for (int i = 0; i < this.getNumberOfWheels(); i++)
-            {
-                this.wheels[i].rpm = 8;
-            }
-        }
-        public void isRoadEmpty(ref List<Vehicle> vehicles)
-        {
-            vehicles.Clear();
+            Console.Write("\n");
+            Console.WriteLine("isEmpty: " + car.isRoadEmpty(ref road));
         }
     }
 }
---
+++ ForClass101/Vehicle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForClass101
+{
+    public class Vehicle
+    {
+        protected Wheel[] wheels;
+        private int fuel;
+
+        public Vehicle(Wheel[] wheels, int fuel)
+        {
+            this.wheels = wheels;
+            this.fuel = fuel;
+        }
+
+        public bool isRoadEmpty(ref List<Vehicle> vehicles)
+        {
+            return (vehicles.Count == 0);
+        }
+
+        public class Wheel
+        {
+            private string kind;
+            private int rpm = 0;
+
+            public Wheel(string kind_p)
+            {
+                this.kind = kind_p;
+            }
+
+            public void SetNRPM(int r)
+            {
+                this.rpm = r;
+            }
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public Car(Wheel[] wheels, int fuel) : base(wheels, fuel) { }
+
+        // 시동을 켠다
+        public void Run()
+        {
+            foreach (Wheel wheel in wheels)
+            {
+                wheel.SetNRPM(5);
+            }
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public Bike(Wheel[] wheels, int fuel) : base(wheels, fuel) { }
+
+        // 페달을 밟는다
+        public void Start()
+        {
+            foreach (Wheel wheel in wheels)
+            {
+                wheel.SetNRPM(8);
+            }
+        }
+    }
+
+}
